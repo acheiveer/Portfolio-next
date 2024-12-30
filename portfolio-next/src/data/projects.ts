afbcd60e7a018c5e1bc19ad50deab3e43d2cff34
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-12-30 23:36:17 +0530

    update on about section

--- portfolio-next/src/data/projects.ts
+++ portfolio-next/src/data/projects.ts
@@ -1,12 +1,12 @@
 import { IProject } from "./types"
-import cfstress from "@/Images/projectImages/cfstress.png"
+import cfstress from "@/Images/ProjectImages/cfstress.png"
 
 const projects: Array<IProject> = [
   {
     title: "CF Stress",
     description: "Community driven, highly customizable, no-code tool to stress test Codeforces problems. Frontend - React, Backend - Golang, Database - MongoDB",
     thumbnail: cfstress,
-    githubLink: "https://github.com/shivam-bhadani/CF-Stress-Backend"
+    githubLink: ""
   },
   
 ]
