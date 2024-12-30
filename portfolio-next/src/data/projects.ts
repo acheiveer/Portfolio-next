48dc3d94c4d99eaecfda660ecdab663ed938e934
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-12-30 23:24:30 +0530

    Added project

---
+++ portfolio-next/src/data/projects.ts
@@ -0,0 +1,14 @@
+import { IProject } from "./types"
+import cfstress from "@/Images/projectImages/cfstress.png"
+
+const projects: Array<IProject> = [
+  {
+    title: "CF Stress",
+    description: "Community driven, highly customizable, no-code tool to stress test Codeforces problems. Frontend - React, Backend - Golang, Database - MongoDB",
+    thumbnail: cfstress,
+    githubLink: "https://github.com/shivam-bhadani/CF-Stress-Backend"
+  },
+  
+]
+
+export default projects
