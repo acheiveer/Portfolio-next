45db6acb34723c2ccd1850b7e6a62ebfd876dd2e
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-12-30 23:50:23 +0530

    bug fix

--- portfolio-next/src/app/projects/page.tsx
+++ portfolio-next/src/app/projects/page.tsx
@@ -5,13 +5,14 @@ const Project = () => {
   return (
     <div className="mt-10 w-[1300px] max-w-[90%] mx-auto">
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 mx-auto">
-        {projects.map((project, index) => (
+        {projects?.map((project, index) => (
           <ProjectCard
             key={index}
-            title={project.title}
-            description={project.description}
-            thumbnail={project.thumbnail}
-            githubLink={project.githubLink}
+            title={project?.title || "No Title"}
+            description={project?.description || "No Description"}
+            thumbnail={project?.thumbnail || ""}
+            githubLink={project?.githubLink || "#"}
+            demoLink={project?.demoLink || "#"}
           />
         ))}
       </div>
