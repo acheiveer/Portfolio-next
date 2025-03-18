50cb0a86af5d4b81046ba0c514463235a79f556b
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2025-03-18 05:48:05 +0530

    Added a project

--- portfolio-next/src/data/projects.ts
+++ portfolio-next/src/data/projects.ts
@@ -1,8 +1,16 @@
 import { IProject } from "./types"
 import AIBlogger from "@/Images/ProjectImages/AIBlogger.png"
 import soon from "@/Images/ProjectImages/soon.webp"
+import ChatApp from "@/Images/ProjectImages/soon.webp"
 
 const projects: Array<IProject> = [
+  {
+    title: "FriendOChats: Interactive Chat Application",
+    description: "Developed a scalable, real-time chat application with MERN, Socket.io, and Zustand, featuring JWT authentication, real-time notifications, and an intuitive UI, boosting engagement by 80%.",
+    thumbnail: ChatApp,
+    githubLink: "https://friendochats.onrender.com/",
+    demoLink: "https://friendochats.onrender.com/"
+  },
   {
     title: "AI Blogger",
     description: "A modern, feature-rich blogging platform designed for writers, readers, and communities to connect, create, and engage. Built with a robust tech stack, this application provides seamless user experience and functionality for blog creation, collaboration, and interaction.",
